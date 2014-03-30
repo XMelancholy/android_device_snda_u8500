f54f85afade04736431fdb99c64a00bb4acdf0e2
xiangxin <xiangxin19960319@gmail.com>
2014-03-30 23:25:41 +0800

    updaye cm.mk

--- cm.mk
+++ cm.mk
@@ -1,9 +1,16 @@
 # Inherit device configuration
 $(call inherit-product, device/snda/u8500/full_u8500.mk)
 
+# Inherit CM common GSM stuff.
+$(call inherit-product, vendor/cm/config/gsm.mk)
+
+# Inherit CM common Phone stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+TARGET_BOOTANIMATION_NAME := 540
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := u8500
-PRODUCT_NAME := aosp_u8500
+PRODUCT_NAME := cm_u8500
 
 # Debug mode
 ADDITIONAL_DEFAULT_PROPERTIES += ro.debuggable=1
