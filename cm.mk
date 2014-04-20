de43d45f607d829a09dda96c4412459b034bfb14
xiangxin <xiangxin19960319@gmail.com>
2014-04-20 23:19:39 +0800

    Android: Fix bootanimation

--- cm.mk
+++ cm.mk
@@ -7,7 +7,9 @@ $(call inherit-product, vendor/cm/config/gsm.mk)
 # Inherit CM common Phone stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-TARGET_BOOTANIMATION_NAME := 540
+TARGET_SCREEN_WIDTH := 540
+TARGET_SCREEN_HEIGHT := 960
+
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := u8500
 PRODUCT_NAME := cm_u8500
